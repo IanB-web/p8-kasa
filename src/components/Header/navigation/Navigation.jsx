b165ef46843be7558b406634f70517628f91d4cb
Ian <deus.nightshade@gmail.com>
2024-02-01 12:25:56 +0100

    Refactor

--- src/components/Header/navigation/Navigation.jsx
+++ src/components/Header/navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import routes from "../../../router/routes";
+import "./navigation.scss"
 
 const Navigation = () => {
   return (
