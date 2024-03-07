d9c4652489ff193aa4dd379735d6f765bbe721f0
Ian <deus.nightshade@gmail.com>
2024-03-07 16:55:26 +0100

    test

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "../../components/header";
-import Footer from "../../components/footer";
-import Error from "../../components/error";
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+import Error from '../../components/error';
 
 const ErrorPage = () => {
   return (
