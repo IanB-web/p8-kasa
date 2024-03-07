38fc650edebd3abc3ab7724bf317072b75731911
Ian <deus.nightshade@gmail.com>
2024-03-07 15:08:47 +0100

    refactor

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/header";
-import Footer from "../../components/footer/index";
+import Footer from "../../components/footer";
 import Error from "../../components/error";
 
 const ErrorPage = () => {
